75af61b6c8c186a5c75a5b1a22fe774f899cd73d
Raselj71 <raselj44@gmail.com>
2024-05-01 14:07:01 +0600

    First Version

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "rasel",
+
+  //gmail credentials for send email
+  EMAIL: "alexkrein9@gmail.com",
+  PASSWORD: "nqtd jdfn rwro piwr",
+
+  //secret key for jwt
+  JWT_SECRET: "rasel",
+
+  //baseURL
+  baseURL: "https://kitmuser.online/",
+
+  //firebase server key for send notification
+  SERVER_KEY:
+    "AAAAlsArBKE:APA91bE0O0ljSKGrslQCANljpNHsvmfV4tbc_5RTL0E10WRhqVq91BvLmlgPFcB45ZM4ojJZYTzFst8zggvJJt4Y5QemuuTLTGLBdwwxR-S5E5LAE4n09VRoEJsD6bfXTp26GXMmM5MP",
+};
