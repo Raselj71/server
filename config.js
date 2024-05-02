f25aa3f956c1f8999799c0d909b31b0a123ac50d
Raselj71 <raselj44@gmail.com>
2024-05-02 13:34:48 +0600

    secondVersion

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
   JWT_SECRET: "rasel",
 
   //baseURL
-  baseURL: "https://kitmuser.online/",
+  baseURL: "http://18.140.6.99/",
 
   //firebase server key for send notification
   SERVER_KEY:
